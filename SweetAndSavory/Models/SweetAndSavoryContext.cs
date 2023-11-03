fe998f3b51bccb04c22669610a883434ca373d20
jessicahattig <jessicahattig@gmail.com>
2023-11-03 16:27:21 -0700

    Update Files names and add boiler plates.

---
+++ SweetAndSavory/Models/SweetAndSavoryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetAndSavory.Models
+{
+  public class SweetAndSavoryContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavors { get; set; }
+
+    public SweetAndSavoryContext(DbContextOptions options) : base(options) { }
+  }
+}
